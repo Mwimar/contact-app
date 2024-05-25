1957e81d52961f7459fed3012a3d87951b4eea36
Mark Mwima <markmwimar@gmail.com>
2024-05-25 03:58:21 +0300

    updated add contact component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
+import React from "react";
 import "./App.css";
+import Header from "./Header.js";
+import AddContact from "./AddContact.js";
+// import ContactList from "./ContactList.js";
 
 function App() {
-  return <div>Wassap</div>;
+  return (
+    <div>
+      <Header />
+      <AddContact />
+      {/* <ContactList /> */}
+    </div>
+  );
 }
 
 export default App;
