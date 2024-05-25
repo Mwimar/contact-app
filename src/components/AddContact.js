1acd294508e3fdf2dfb689a46bb86338c77aff70
Mark Mwima <markmwimar@gmail.com>
2024-05-25 04:01:23 +0300

    added save button

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -14,6 +14,7 @@ class AddContact extends React.Component {
             <label>Email</label>
             <input type="text" name="email" placeholder="Email" />
           </div>
+          <button className="ui button blue">Add Contact</button>
         </form>
       </div>
     );
