f208a83e0c87d440306eef1e3019ef37d0818bf3
Mark Mwima <markmwimar@gmail.com>
2024-05-25 04:06:36 +0300

    added contact list function

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./Header.js";
 import AddContact from "./AddContact.js";
-// import ContactList from "./ContactList.js";
+import ContactList from "./ContactList.js";
 
 function App() {
   return (
     <div>
       <Header />
       <AddContact />
-      {/* <ContactList /> */}
+      <ContactList />
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactList = () => {
+  return <div className="ui celled list"></div>;
+};
+
+export default ContactList;
